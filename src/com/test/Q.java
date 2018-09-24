d708b087abb2c337ffb377077ea29b10eb0bfd80
hanrx <531186370@qq.com>
2018-09-24 14:24:44 +0800

    ss

--- src/com/test/Q.java
+++ src/com/test/Q.java
@@ -5,6 +5,7 @@ public class Q {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("111111111");
+		System.out.println("22222222222222222");
 	}
 
 }
--- src/com/test/W.java
+++ src/com/test/W.java
@@ -4,13 +4,11 @@ public class W {
 
 	@Override
 	public boolean equals(Object arg0) {
-		// TODO Auto-generated method stub
 		return super.equals(arg0);
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return super.toString();
 	}
 }
