716abeae97b87db63d52e5a3bc47de9c6daa4de9
hanrx <531186370@qq.com>
2018-09-24 13:21:38 +0800

    EE

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -3,7 +3,7 @@ package com.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("=============Test");
+		System.out.println("=11============TestTest");
 	}
 
 }
