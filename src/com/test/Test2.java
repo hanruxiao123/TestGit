cc80706e37ef24d66ddf769ad3eabba27c6844c3
hanrx <531186370@qq.com>
2018-09-24 13:37:21 +0800

    ss

--- src/com/test/Test2.java
+++ src/com/test/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("==============Test2Test2");
+		System.out.println("==============Test2Test2Test2");
 	}
 
 }
