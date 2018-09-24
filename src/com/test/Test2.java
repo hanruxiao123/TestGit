282893d0ea6cd04841623963c271fb79754d674a
hanrx <531186370@qq.com>
2018-09-24 11:57:14 +0800

    dd

--- src/com/test/Test2.java
+++ src/com/test/Test2.java
@@ -1,10 +1,10 @@
 package com.test;
 
-public class Test {
+public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("=============Test");
+		System.out.println("==============Test2");
 	}
 
 }
